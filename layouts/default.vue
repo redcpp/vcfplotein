51a75720ecd1a7ec89da6122c5ef65fb9cdfac58
Diego Said Anaya Mancilla <redcpp@gmail.com>
2018-12-09 16:08:32 -0600

    Add global spinner before mount

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,29 @@
 <template>
-  <div id="default-wrapper">
+  <div id="default-wrapper" v-if="loaded">
     <navbar />
     <nuxt/>
+  </div>
+  <div class="row h-100 justify-content-center align-items-center" v-else>
+    <spinner />
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
+import Spinner from '~/components/Spinner.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Spinner
+  },
+  data: () => ({
+    loaded: false
+  }),
+  mounted () {
+    this.$nextTick(() => {
+      this.loaded = true
+    })
   }
 }
 </script>
